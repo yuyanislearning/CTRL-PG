e0092d811fe0597240f1c9fe0f5ba3d913859113
Yichao Zhou <yichao.zhou@oath.com>
2020-01-09 17:51:29 -0800

    start doc

--- examples/extra_layers.py
+++ examples/extra_layers.py
@@ -1178,12 +1178,12 @@ class BertForSequenceClassification(BertPreTrainedModel):
         return outputs  # (loss), logits, (hidden_states), (attentions)
 
 
-@add_start_docstrings(
-    """Bert Model transformer with a relation classification/regression head on top (a linear layer on top of
-                      the pooled output) e.g. for GLUE tasks. """,
-    BERT_START_DOCSTRING,
-    BERT_INPUTS_DOCSTRING,
-)
+# @add_start_docstrings(
+#     """Bert Model transformer with a relation classification/regression head on top (a linear layer on top of
+#                       the pooled output) e.g. for GLUE tasks. """,
+#     BERT_START_DOCSTRING,
+#     BERT_INPUTS_DOCSTRING,
+# )
 class BertForRelationClassification(BertPreTrainedModel):
 
     def __init__(self, config):
@@ -1229,11 +1229,12 @@ class BertForRelationClassification(BertPreTrainedModel):
         return outputs  
 
 
-## Self-Defined Graph Model
-@add_start_docstrings("""Bert Model transformer with a node embedding head on top (a linear layer on top of
-                     the pooled output) e.g. for I2B2 tasks. """,
-                     BERT_START_DOCSTRING,
-                     BERT_INPUTS_DOCSTRING)
+# ## Self-Defined Graph Model
+# @add_start_docstrings("""Bert Model transformer with a node embedding head on top (a linear layer on top of
+#                      the pooled output) e.g. for I2B2 tasks. """,
+#                      BERT_START_DOCSTRING,
+#                      BERT_INPUTS_DOCSTRING,
+# )
 class BertForNodeEmbedding(BertPreTrainedModel):
 
     def __init__(self, config):
@@ -1258,45 +1259,45 @@ class BertForNodeEmbedding(BertPreTrainedModel):
 
 
 
-# @add_start_docstrings("""Bert Model transformer with a relation classification/regression head on top (a linear layer on top of
-#                      the pooled output) e.g. for GLUE tasks. """,
-#                      BERT_START_DOCSTRING,
-#                      BERT_INPUTS_DOCSTRING)
-class BertForRelationClassification(BertPreTrainedModel):
+# # @add_start_docstrings("""Bert Model transformer with a relation classification/regression head on top (a linear layer on top of
+# #                      the pooled output) e.g. for GLUE tasks. """,
+# #                      BERT_START_DOCSTRING,
+# #                      BERT_INPUTS_DOCSTRING)
+# class BertForRelationClassification(BertPreTrainedModel):
 
-    def __init__(self, config):
-        super(BertForRelationClassification, self).__init__(config)
-        self.num_labels = config.num_labels
-        self.dropout = nn.Dropout(config.hidden_dropout_prob)
-        self.classifier = nn.Linear(config.hidden_size, self.config.num_labels)
-        self.init_weights()
+#     def __init__(self, config):
+#         super(BertForRelationClassification, self).__init__(config)
+#         self.num_labels = config.num_labels
+#         self.dropout = nn.Dropout(config.hidden_dropout_prob)
+#         self.classifier = nn.Linear(config.hidden_size, self.config.num_labels)
+#         self.init_weights()
 
-    def forward(self, input_ids=None, attention_mask=None, token_type_ids=None,
-                position_ids=None, head_mask=None, inputs_embeds=None, labels=None):
+#     def forward(self, input_ids=None, attention_mask=None, token_type_ids=None,
+#                 position_ids=None, head_mask=None, inputs_embeds=None, labels=None):
 
-        outputs = self.bert(input_ids,
-                            attention_mask=attention_mask,
-                            token_type_ids=token_type_ids,
-                            position_ids=position_ids,
-                            head_mask=head_mask,
-                            inputs_embeds=inputs_embeds)
+#         outputs = self.bert(input_ids,
+#                             attention_mask=attention_mask,
+#                             token_type_ids=token_type_ids,
+#                             position_ids=position_ids,
+#                             head_mask=head_mask,
+#                             inputs_embeds=inputs_embeds)
 
-        pooled_output = outputs[1]
-        pooled_output = self.dropout(pooled_output)
-        logits = self.classifier(pooled_output)
-        outputs = (logits,) + outputs[2:]  # add hidden states and attention if they are here
+#         pooled_output = outputs[1]
+#         pooled_output = self.dropout(pooled_output)
+#         logits = self.classifier(pooled_output)
+#         outputs = (logits,) + outputs[2:]  # add hidden states and attention if they are here
 
-        if labels is not None:
-            if self.num_labels == 1:
-                #  We are doing regression
-                loss_fct = MSELoss()
-                loss = loss_fct(logits.view(-1), labels.view(-1))
-            else:
-                loss_fct = CrossEntropyLoss()
-                loss = loss_fct(logits.view(-1, self.num_labels), labels.view(-1))
-            outputs = (loss,) + outputs
+#         if labels is not None:
+#             if self.num_labels == 1:
+#                 #  We are doing regression
+#                 loss_fct = MSELoss()
+#                 loss = loss_fct(logits.view(-1), labels.view(-1))
+#             else:
+#                 loss_fct = CrossEntropyLoss()
+#                 loss = loss_fct(logits.view(-1, self.num_labels), labels.view(-1))
+#             outputs = (loss,) + outputs
 
-        return outputs  # (loss), logits, (hidden_states), (attentions)
+#         return outputs  # (loss), logits, (hidden_states), (attentions)
 
 import numpy as np
 from torch_geometric.nn import SAGEConv, GATConv
