3ecc312313378c2c5892d35586d1c8c945d77105
Yichao Zhou <yichao.zhou@oath.com>
2020-01-19 16:49:54 -0800

    train

--- examples/extra_layers_2.py
+++ examples/extra_layers_2.py
@@ -53,7 +53,7 @@ class BertForRelationClassification(nn.Module):
         if label is not None:
 
             loss_fct = CrossEntropyLoss()
-            loss = loss_fct(logits.view(-1, self.num_labels), labels.view(-1))
+            loss = loss_fct(logits.view(-1, self.num_labels), label.view(-1))
             outputs = (loss,) + outputs
 
         return outputs  
